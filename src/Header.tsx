323f023a12cca5bd54010a4142319f08dbee1cbb
Nishant Bhargava <nishantbhargava@Nishants-Mac-mini.local>
2024-08-20 16:27:42 +0530

    Removed unwanted code

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,29 +1,3 @@
-// import React, { useState } from 'react';
-// import './index.css';
-// import Hamburger from './Hamburger';
-
-// interface HeaderProps {
-//   routeName: string;
-// }
-
-// export default function Header({ routeName }: HeaderProps) {
-//     const [message, setMessage] = useState('');
-
-//   const passInfo2Routes = (value: string) => {
-//     setMessage(value);
-//     routeName = value;
-//   };
-
-//   return (
-//     <header className="header">
-//       <Hamburger shareNavigationInfo={passInfo2Routes} />
-//       <div className="header-text">
-//         <h1>{message ? message : "Products"}</h1>
-//       </div>
-//     </header>
-//   );
-// }
-
 import React from 'react';
 import './index.css';
 import Hamburger from './Hamburger';
