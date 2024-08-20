21af8fb929ec4d49b65dd99e2dd9d072c41ced45
Nishant Bhargava <nishantbhargava@Nishants-Mac-mini.local>
2024-08-20 17:16:16 +0530

    Network Monitor Added

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,25 @@ import Product from 'pages/Product';
 import Categories from 'pages/Categories';
 import Debugging from 'second/Debugging';
 
+import NetworkMonitor from './NetworkMonitor';
+
 const App = () => {
   const [route, setRoute] = useState('Products'); // Default route is "Products"
+
+  const handleDataCapture = (data) => {
+    // We can send this data as payload and share it with our internal analytical servers to evaluate it better.
+    console.log(data);
+  };
 
   return (
     <>
       <Header setRoute={setRoute} currentRoute={route} />
       <section>
+        <NetworkMonitor onCapture={handleDataCapture} />
         {route === 'Products' && <Product />}
         {route === 'Categories' && <Categories />}
         {route === 'Debugging' && <Debugging />}
-        {route === 'Contact' && <div>Contact Page</div>} {/* Add a Contact component if needed */}
+        {route === 'Contact' && <div>Contact Page</div>}
       </section>
       <Footer />
     </>
@@ -28,6 +36,6 @@ const App = () => {
 const rootElement = document.getElementById('app');
 if (!rootElement) throw new Error('Failed to find the root element');
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(<App />);
